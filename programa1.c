3a5081b0d89474ee541be769db34c5abc3c5f4aa
daniloti1 <danilo.ti@hotmail.com>
2017-05-20 17:36:51 -0500

    Ahora funciona con mayusculas

--- programa1.c
+++ programa1.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -5,12 +6,13 @@
 
 char* cifrar(char cadena[30], int n);
 int buscar(char a, char texto[30]);
+int verificar(char letra);
 
 int main(int argc, char **argv){
 	printf("Cifrado ciclico\n");
 	printf("Ingrese el mensaje a cifrar: \n");
 	char cadenaCode[30];
-	scanf("%[a-z 0-9!#$%&\"\\/()={}]", cadenaCode);
+	scanf("%[a-z A-Z0-9.,@_|°!#$%&\"\\/()={}]", cadenaCode);
 	printf("Ingrese la clave numerica: \n");
 	int num;
 	scanf("%d", &num);
@@ -26,12 +28,18 @@ char* cifrar(char cadena[30], int n){
         char cifrado[30];
 	strcpy(cifrado,cadena);
 	char alfabeto[26]="abcdefghijklmnopqrstuvwxyz";
+	char alfabetoM[26]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         int a=(int)strlen(cadena);
 	for (int i=0;i<a;i++){
-		int b=buscar(cadena[i],alfabeto);
-		if (b!=-1) {
+		if (verificar(cifrado[i])==0){
+			int b=buscar(cifrado[i],alfabeto);
 			b=(b+n)%26;
-                	cifrado[i]=alfabeto[b];
+			cifrado[i]=alfabeto[b];
+		}
+		else if (verificar(cifrado[i])==1){
+			int b=buscar(cifrado[i],alfabetoM);
+			b=(b+n)%26;
+			cifrado[i]=alfabetoM[b];
 		}
 	}
 	strcpy(cifradoFinal,cifrado);
@@ -50,4 +58,14 @@ int buscar(char a, char texto[30]){
 		indice=-1;
 	}
 	return indice;
+}
+
+int verificar(char letra){
+	if (letra>='a' && letra<='z'){
+		return 0;
+	}
+	if (letra>='A' && letra<='Z'){
+		return 1;
+	}
+	return -1;
 }
