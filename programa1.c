7de54df92fd65e18520725af71df3baf22c3da29
daniloti1 <danilo.ti@hotmail.com>
2017-05-20 14:52:14 -0500

    correcion de errores

--- programa1.c
+++ programa1.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 char cifrar(char cadena[30], int n);
-int buscar(char a, cahr texto[30]);
+int buscar(char a, char texto[30]);
 
 int main(int argc, char **argv){
 	printf("Cifrado ciclico\n");
 	printf("Ingrese el mensaje a cifrar: \n");
-	char cadena[30]="mensaje";
-	scanf("%s", cadena);
+	char cadenaCode[30]="mensaje";
+	scanf("%s", cadenaCode);
 	printf("Ingrese la clave numerica: \n");
 	int num=0;
 	scanf("%d", &num);
 	
-	printf("Mensaje cifrado: %s \n", cifrar(cadena));
+	printf("Mensaje cifrado: %s \n", cifrar(cadenaCode,num));
 
 	return 0;
 }
@@ -20,13 +21,14 @@ int main(int argc, char **argv){
 char cifrar(char cadena[30], int n){
 	char cifrado[30]="";
         char alfabeto[26]="abcdefghijklmnopqrstuvwxyz";
-        for (int i=0;strlen(cadena);i++){
-                cifrado[i]=alfabeto[
-
+        int a=(int)strlen(cadena);
+	for (int i=0;a;i++){
+                cifrado[i]=alfabeto[(buscar(cadena[i],alfabeto)+n)%26];
+	}
 	return cifrado;
 }
 
-int buscar(char a, char tetxo[30]){
+int buscar(char a, char texto[30]){
 	int indice=-1;
 	for (int i=0;strlen(texto);i++){
 		if (a==texto[i]){
