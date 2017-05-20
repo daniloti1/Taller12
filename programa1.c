305ddee5375351ec0c68ecbaa47ff8bde6e4a2e4
daniloti1 <danilo.ti@hotmail.com>
2017-05-20 14:11:06 -0500

    correcion de erro faltaba );

--- programa1.c
+++ programa1.c
@@ -3,12 +3,13 @@
 int main(int argc, char **argv){
 	printf("Cifrado ciclico\n");
 	printf("Ingrese el mensaje a cifrar: \n");
-	char cadena[10];
+	char cadena[10]="mensaje";
 	scanf("%s", cadena);
-	printf("Ingrese la clave numerica: \n"
-	int i;
+	printf("Ingrese la clave numerica: \n");
+	int i=0;
 	scanf("%d", &i);
 	
-	printf("Mensaje cifrado: $cadena");
+	printf("Mensaje cifrado: %s \n", cadena);
+
 	return 0;
 }
