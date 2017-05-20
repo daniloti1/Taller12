211a919dd11ca3739e89e801b27f20d36fe94b80
daniloti1 <danilo.ti@hotmail.com>
2017-05-20 13:42:04 -0500

    primer commit

---
+++ programa1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+	printf("Cifrado ciclico\n");
+	printf("Ingrese el mensaje a cifrar: \n");
+	char cadena[10];
+	scanf("%s", cadena);
+	printf("Ingrese la clave numerica: \n"
+	int i;
+	scanf("%d", &i);
+	
+	printf("Mensaje cifrado: ");
+	return 0;
+}
