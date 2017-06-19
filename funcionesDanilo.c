8121b09509eaf35810fe1274c461f1668af41aad
daniloti1 <danilo.ti@hotmail.com>
2017-06-19 00:01:07 -0500

    Correcciones varias

--- funcionesDanilo.c
+++ funcionesDanilo.c
@@ -2,15 +2,15 @@
 #include <miLista.h>
 
 int Lista_Inicializar(ListaEnlazada *lista){
-	if (lista->ancla != NULL) {
+	if (lista->ancla.objeto != NULL) {
 		return FALSE;
 	}
 	lista->numeroElementos = 0;
-	ElementoLista inicio = (Elementolista) malloc(sizeof(ElementoLista)); 
+	ElementoLista* inicio = (Elementolista*) malloc(sizeof(ElementoLista*)); 
 	inicio->objeto = &lista->numeroElementos;
 	inicio->anterior = NULL;
 	inicio->siguiente = NULL;
-	lista->ancla = inicio;
+	lista->ancla = *inicio;
 	return TRUE;
 }
 
@@ -29,7 +29,7 @@ int Lista_Conteo(ListaEnlazada *lista){
 ElementoLista *Lista_Buscar(ListaEnlazada *lista, void *objeto){
 	ElementoLista* elem = &lista->ancla;
 	for (int i = 0; i<lista->numeroElementos; i++) {
-		if (*objeto == *elem->objeto) {
+		if (objeto == elem->objeto) {
 			return elem;
 		}
 		elem = elem->siguiente;
@@ -42,52 +42,46 @@ void Lista_Sacar(ListaEnlazada *lista, ElementoLista *elemento){
 	if (elem == NULL) {
 		return;
 	}
-	elem->siguiente->anterior = ele->anterior;
+	elem->siguiente->anterior = elem->anterior;
 	elem->anterior->siguiente = elem->siguiente;
 	elem->siguiente = NULL;
 	elem->anterior = NULL;
 	lista->numeroElementos -= 1;
-	lista->ancla->objeto = &lista->numeroElementos;
+	lista->ancla.objeto = &lista->numeroElementos;
 	//free(elem); de esta liena no estoy seguro
 }
 
 void Lista_SacarTodos(ListaEnlazada *lista){
-	lista->ancla-siguiente = NULL;
+	lista->ancla.siguiente = NULL;
 	lista->numeroElementos = 0;
-	lista->ancla->objeto = &lista->numeroElementos;
+	lista->ancla.objeto = &lista->numeroElementos;
 }
 
 int Lista_InsertarDespues(ListaEnlazada *lista, void *objeto, ElementoLista *elemento){
-	if (lista->ancla == NULL) {
-		return FALSE;
-	}
 	ElementoLista* elem = Lista_Buscar(lista, elemento->objeto);
 	if (elem == NULL) {
 		return FALSE;
 	}
 	elemento->anterior = elem;
-	elemento->siguietne = elem->siguiente;
-	elem-siguiente->anterior = elemento;
-	elem-siguiente = elemento;
+	elemento->siguiente = elem->siguiente;
+	elem->siguiente->anterior = elemento;
+	elem->siguiente = elemento;
 	lista->numeroElementos += 1;
-	lista->ancla->objeto = &lista->numeroElementos;
+	lista->ancla.objeto = &lista->numeroElementos;
 	return TRUE;	
 }
 
 int Lista_InsertarAntes(ListaEnlazada *lista, void *objeto, ElementoLista *elemento){
-	if (lista->ancla == NULL) {
-		return FALSE;
-	}
 	ElementoLista* elem = Lista_Buscar(lista, elemento->objeto);
 	if (elem == NULL) {
 		return FALSE;
 	}
 	elemento->anterior = elem->anterior;
-	elemento->siguietne = elem;
+	elemento->siguiente = elem;
 	elem->anterior->siguiente = elemento;
 	elem->anterior = elemento;
 	lista->numeroElementos += 1;
-	lista->ancla->objeto = &lista->numeroElementos;
+	lista->ancla.objeto = &lista->numeroElementos;
 	return TRUE;
 
 }
