6625c08a3e5355b63450c262dd74e956deea1b84
daniloti1 <danilo.ti@hotmail.com>
2017-06-19 00:10:39 -0500

    cambiando <> por comillas en el include

--- funcionesDanilo.c
+++ funcionesDanilo.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <miLista.h>
+#include "miLista.h"
 
 int Lista_Inicializar(ListaEnlazada *lista){
 	if (lista->ancla.objeto != NULL) {
 		return FALSE;
 	}
 	lista->numeroElementos = 0;
-	ElementoLista* inicio = (Elementolista*) malloc(sizeof(ElementoLista*)); 
+	ElementoLista* inicio;// = (Elementolista*) malloc(sizeof(ElementoLista*)); 
 	inicio->objeto = &lista->numeroElementos;
 	inicio->anterior = NULL;
 	inicio->siguiente = NULL;
