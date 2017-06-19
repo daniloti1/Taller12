aa7be29cefdd09988fb75ce9cf82bd7508db7cd4
Jealvia <jealvia@espol.edu.ec>
2017-06-18 22:11:05 -0500

    Funciones Julio

---
+++ funcionesJulio.c
@@ -0,0 +1,12 @@
+extern int Lista_Vacia(ListaEnlazada *lista);
+extern int Lista_Conteo(ListaEnlazada *lista);
+
+
+extern int Lista_InsertarFin(ListaEnlazada *lista, void *objeto);
+extern int Lista_InsertarInicio(ListaEnlazada *lista, void *objeto);
+$entoLista *elemento);
+
+
+extern ElementoLista *Lista_Primero(ListaEnlazada *lista);
+extern ElementoLista *Lista_Ultimo(ListaEnlazada *lista);
+
