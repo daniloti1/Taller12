0da38569c193801a9a6c87c5ba066e67f842afeb
daniloti1 <danilo.ti@hotmail.com>
2017-06-23 18:34:55 -0500

    corrigiendo errores

--- src/Lista_InsertarFin.c
+++ src/Lista_InsertarFin.c
@@ -5,18 +5,18 @@ int Lista_InsertarFin(ListaEnlazada *lista, void *objeto){
 	ElementoLista *elemento;	
 	if(lista->numeroElementos==0){	
 		elemento->objeto=objeto;
-		elemento->siguiente = lista->ancla
-		elemento->anterior = lista->ancla;
-		lista->ancla->siguiente = elemento;
-		lista->ancla->anterior = elemento;
-		lista->ancla->numeroElementos++;
+		elemento->siguiente = &lista->ancla;
+		elemento->anterior = &lista->ancla;
+		lista->ancla.siguiente = elemento;
+		lista->ancla.anterior = elemento;
+		lista->numeroElementos++;
 		return TRUE;
 	}else{
 		elemento->objeto=objeto;
-		elemento->siguiente = lista->ancla
+		elemento->siguiente = &lista->ancla;
 		elemento->anterior = lista->anterior;
-		lista->ancla->anterior = elemento;
-		lista->ancla->numeroElementos++;	
+		lista->ancla.anterior = elemento;
+		lista->numeroElementos++;	
 		return TRUE;
 	}
 	return FALSE;
