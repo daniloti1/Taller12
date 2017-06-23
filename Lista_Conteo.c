876c849709d95ad3521ea05a587145ccb6460527
daniloti1 <danilo.ti@hotmail.com>
2017-06-23 18:05:11 -0500

    borrando del directorio

--- Lista_Conteo.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "miLista.h"
-
-int Lista_Conteo(ListaEnlazada *lista){
-	return lista->numeroElementos;
-}
--- Lista_SacarTodos.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "miLista.h"
-
-void Lista_SacarTodos(ListaEnlazada *lista){
-	lista->ancla.siguiente = NULL;
-	lista->numeroElementos = 0;
-	lista->ancla.objeto = &lista->numeroElementos;
-}
-
