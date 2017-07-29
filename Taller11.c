09ce1e7537ec41c3d0205bd5ed3cd6ae2461bc1b
daniloti1 <danilo.ti@hotmail.com>
2017-07-29 18:28:59 -0500

    Funciona solo falta validar lo de si no es un numero de elementos pares

--- Taller11.c
+++ Taller11.c
@@ -1,123 +1,200 @@
 #include <stdio.h>
+
 #include <time.h>
+
 #include <stdlib.h>
+
 #include <sys/time.h>
+
 #include <pthread.h>
+
 
 
 int aleatorio(int min, int max){
 
+
 	return (rand() % (max-min+1)) + min;
 
+
 }
+
 
 double obtenerTiempoActual(){
 
+
 	struct timespec tsp;
+
 
 	clock_gettime( CLOCK_REALTIME, &tsp);
 
+
 	double secs = (double)tsp.tv_sec;
+
 	double nano = (double)tsp.tv_nsec / 1000000000.0;
+
 
 	return secs + nano;
 
+
 }
 
+
 typedef struct _apy {
+
 	int * arreglo;
+
 	//int tamano;
+
 	int inicio;
+
 	int fin;
+
 } apy;
 
+
 void * funcion(void* estructura) {
+
 	//int tamano = estructura->tamano;
+
 	apy * estructura2 = (apy*) estructura;
+
 	int inicio = (int)estructura2->inicio;
+
 	int fin = (int)estructura2->fin;
+	printf("Fin: %d\n",fin);
 
 	int * arreglo = (int *)estructura2->arreglo;
 
+
 	int suma = 0;
 
+
 	for (int i = inicio; i < fin; i++) {
+
 		suma += arreglo[i];
+
 	}
 
+
 	return (void *)suma;
+
 }
+
 
 
 int main(int argc, char **argv) {
 
+
 	srand(time(0));
-	
+
+
 	if (argc < 3) {
+
 		printf("./Taller11 <tamaño_arreglo> <numero_hilos>");
+
 		exit(1);
+
 	}
 
-	double tiempo_0 = obtenerTiempoActual();
 
 	int tamano = atoi(argv[1]);
 
+
 	int hilos = atoi(argv[2]);
 
+
 	int * arreglo = (int *) malloc(sizeof(int)*tamano);
+	
+	int numero = 0;
+
 	for (int i = 0; i < tamano; i++) {
+
 		*(arreglo+i) = aleatorio(50,200);
+		numero += *(arreglo+i);
+
 	}
-	
+	printf("Numero que deberia salir: %d\n",numero);
+
+
 	int numHilos = tamano/hilos;
-	
+
+
 	int sumaFinal = 0;
-	
-	
-	for (int j = 0; j < tamano; j += numHilos) {
+
+    	pthread_t * listaIds = (pthread_t *) malloc(sizeof(pthread_t)*hilos);
+
+	int contador = 0;
+
+	double tiempo_0 = obtenerTiempoActual();
+
+	for (int j = 0; j < tamano; j += numHilos) {		
+
 		pthread_t id;
-		
 
-		apy  est;
-		est.inicio = j;
-		est.fin = j + numHilos;
+
+
+		apy * estructura = (apy *)malloc(sizeof(apy));
+
+		estructura->inicio = j;
+
+		estructura->fin = j + numHilos;
+		printf("Fin estructura: %d\n",estructura->fin);
 		//estructura->tamano = ;
-		est.arreglo = arreglo;
-		apy * estructura = &est;
 
-		int statusHilo = pthread_create(&id, NULL, funcion, (void *)estructura);
+		estructura->arreglo = arreglo;
+
+		//apy * estructura = &est;
+
+
+		int statusHilo = pthread_create(&listaIds[contador], NULL, funcion, (void *)estructura);
+
 
 		if (statusHilo < 0) {
+
 			fprintf(stderr, "Error al crear el hilo\n");
+
 			exit(-1);	
+
 		}
+
 		//funcion(&estructura);
 
-		void * retorno = NULL;
+		contador++;
 
-		int status = pthread_join(id, &retorno);
-		
+	}
+
+	for (int k = 0; k < hilos; k++) {
+		printf("Dentro del For: %d\n",k);
+
+		void * retorno = NULL;
+		printf("Id del hilo: %d\n",listaIds[k]);
+
+		int status = pthread_join(listaIds[k], &retorno);
+
+		printf("se hizo el join:\n");	
 		if (status < 0) {
+
 			fprintf(stderr, "Error al crear el hilo\n");
+
 			exit(-1);	
+
 		}
 
+
 		printf("Valor de Retorno: %lu\n",(long)retorno);
+
 		sumaFinal += (int) retorno;
 
 	}
 
-	
 
 	double tiempo_1 = obtenerTiempoActual();
+
 	double tiempoTotal = tiempo_1 - tiempo_0;
+
 	printf("Suma Total: %d\n",sumaFinal);
 
+
 	printf("Tiempo de ejecucion: %9f\n",tiempoTotal);
+
 }
-
-
-
-
-
-
