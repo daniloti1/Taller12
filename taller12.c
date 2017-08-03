5dda42a7934ae37f232ad1465f4888824446fb9e
tugfa <tugfa@gmail.com>
2017-08-03 16:20:11 -0500

    primer commit

---
+++ taller12.c
@@ -0,0 +1,31 @@
+
+
+
+
+int numero_lineas(char*ruta,int*tam_lineas){
+	if(ruta!=NULL){
+		FILE*ar=fopen(ruta,"r");
+		int lineas=0;
+		int tam_linea;
+		while(!feof(ar)){
+			tam_linea++;
+			char c=getc(ar);
+			if(c=='\n'){
+				if(tam_lineas!=NULL){
+					tam_lineas[lineas]=tam_linea;
+				}
+				lineas++;
+				tam_linea=0;
+			}
+		}
+		fclose(ar);
+		return lineas;
+	}
+	return -1;
+}
+
+main(int arg c){
+	
+
+
+}
