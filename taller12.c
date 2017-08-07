1961002cf98a634a91c77c978f9e303941fa85f8
daniloti1 <danilo.ti@hotmail.com>
2017-08-06 21:46:54 -0500

    no da warnings

--- taller12.c
+++ taller12.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <pthread.h>
+#include <string.h>
+
 
 #define MAX 1000000;
 
@@ -29,55 +31,103 @@ int numero_lineas(char*ruta,int*tam_lineas){
 	return -1;
 }
 
-void * funcion(void* count) {
+typedef struct _apy {
+
+	char ** palabras;
+
+	int * numpalabras;
+
+	char * parrafo;
+
+} apy;
+
+
+void * funcion(void* estructura) {
 	pthread_mutex_t mutex;
-	
+	apy * estructura2 = (apy*) estructura;
+
+	char *str = (char *) estructura2->parrafo;
+	char ** palabras = estructura2->palabras;
+	int * num_palabras = estructura2->numpalabras;
+	char *token;
+	token = strtok(str, " \"!·$%&/()=º|@#~½¬<>-_'ç`+*[]{}ḉç¿,.!?:;");
+
+	while (token != NULL) {
+	if (!strcmp(*(palabras), token)) {
+    		pthread_mutex_lock(&mutex);
+		*(num_palabras) = *(num_palabras) + 1;
+		pthread_mutex_unlock(&mutex);
+	} else if (!strcmp(*(palabras + 1), token)) {
+		pthread_mutex_lock(&mutex);
+		*(num_palabras + 1) = *(num_palabras + 1) + 1;
+		pthread_mutex_unlock(&mutex);
+	} else if (!strcmp(*(palabras + 2), token)) {
+		pthread_mutex_lock(&mutex);
+		*(num_palabras + 2) = *(num_palabras + 2) + 1;
+		pthread_mutex_unlock(&mutex);
+	}
+	token = strtok(NULL, " \"!·$%&/()=º|@#~½¬<>-_'ç`+*[]{}ḉç¿,.!?:;");
+	}
+
+	printf("--*-Palabras encontradas-*-- \nPalabra: %s - > Cantidad: %d\nPalabra: %s - > Cantidad: %d\nPalabra: %s - > Cantidad: %d\n", *(palabras), *(num_palabras), *(palabras + 1), *(num_palabras + 1), *(palabras + 2), *(num_palabras + 2));
+	return (void *) 0;
+
 }
 
-main(int argc, char ** argv){
+
+int main(int argc, char ** argv){
 	char ** palabras = (char**)malloc(sizeof(char*)*(argc-3));
-	int ** numpalabras = (int**)malloc(sizeof(int*)*(argc-3));
-	char *ruta=argv[1];
-	int numHilos=atoi(argv[2]);
-	int numeroLineas=numero_lineas(ruta,0);
-	int numCaraterXlinea*=malloc(sizeof(int)*numeroLineas);
-	numero_lineas(ruta,numCaracteresLinea);
-	int totPalabraas=0;
+	int * numpalabras = (int*)malloc(sizeof(int)*(argc-3));
+	char *ruta = argv[1];
+	int numHilos = atoi(argv[2]);
+	int numLineas = numero_lineas(ruta,0);
+	int * numCaracteresXLinea = malloc(sizeof(int)*numLineas);
+	numero_lineas(ruta,numCaracteresXLinea);
+	int totPalabraas = 0;
 	
+	char **parrafos = malloc(numHilos*sizeof(char *));
+
 	for (int i  = 0; i < argc-3; i++) {
 		*(palabras+i) = argv[i+3];
 	}
 	
 	for(int j = 1; j < numLineas ; j++){
-        totPalabraas  += *(numCaracterXlinea+j);
+        totPalabraas  += *(numCaracteresXLinea+j);
     }
 	
 	int divisionParaCadaHilo=totPalabraas/numHilos;
 	
 	int inicioParaElHilo=0;
 	
-	for(int k=0;k<numHilos;k++){
+	for(int k = 0; k < numHilos; k++){
 		FILE* fp=fopen(ruta,"r");//apertura de archivo
 		
-        fseek(fp, inicioParaElHilo,SEEK_SET); 
-        *(palabras+k) = malloc(divisionParaCadaHilo*sizeof(char));
+		fseek(fp, inicioParaElHilo,SEEK_SET); 
+		*(palabras+k) = malloc(divisionParaCadaHilo*sizeof(char));
 
-        for( int j = 0 ; j < divisionParaCadaHilo ; j++){
-            char c = getc(file);
-            *(*(parrafos+k)+j) = c; 
-        }      
-        seek = seek + bloque;
+		for( int j = 0 ; j < divisionParaCadaHilo; j++){
+		    char c = getc(fp);
+		    *(*(parrafos+k)+j) = c; 
+		}      
+		inicioParaElHilo += divisionParaCadaHilo;
 
 	}
-	pthread_t * listaIds = (pthread_t *) malloc(sizeof(pthread_t)*hilos);
+
+	pthread_t * listaIds = (pthread_t *) malloc(sizeof(pthread_t)*numHilos);
 	
 	
 	
-	char*linea=fgets(fp,buf,);
+	//char*linea=fgets(fp,buf,);
 	
 	//Se crea los hilos necesarios
-	for(int j=0;j<sizeof(palabras);j++){
-		int statusHilo = pthread_create(&listaIds[contador], NULL, funcion, NULL);
+	for(int j=0; j < numHilos; j++){
+		apy * estructura = (apy *)malloc(sizeof(apy));
+		estructura->parrafo = parrafos[j];
+		estructura->palabras = palabras;
+		estructura->numpalabras = numpalabras;
+		
+
+		int statusHilo = pthread_create(&listaIds[j], NULL, funcion, (void *) estructura);
 		if (statusHilo < 0) {
 			fprintf(stderr, "Error al crear el hilo\n");
 			exit(-1);	
@@ -85,6 +135,16 @@ main(int argc, char ** argv){
 		
 		
 	}
+
+	for (int j = 0; j < numHilos; j++) {
+        int statusHiloJoin = pthread_join(listaIds[j], NULL);
+        if (statusHiloJoin < 0) {
+            fprintf(stderr, "Error al esperar por el hilo\n");
+            exit(-1);
+        }
+    }
+
+	return (0);
 		
 	
 	
